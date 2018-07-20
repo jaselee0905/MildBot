7d7220a5469d35ad52513ad860b8d8921a0e2c9a
jaselee0905 <me@jaesunglee.ca>
2018-07-19 23:09:42 -0400

    Mild Bot
    First version of Mild bot

---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const bot = new discord.Client();
+bot.commands = new discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    bot.commands.set(command.name, command);
+}
+
+bot.on('ready', () => {
+    console.log('Ready!');
+});
+/*
+bot.registry.registerGroup('random', 'Random');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+*/
+
+bot.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (!bot.commands.has(command)) return;
+
+    try {
+        bot.commands.get(command).execute(message, args);
+    }
+    catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
+});
+
+bot.login(token);
